fa7ec1529bd35b71736ca468fa24179ac97494b2
Vladislav <adrenalin_1231@mail.ru>
2020-11-26 22:28:04 +0300

    ewgweg

---
+++ Individual3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import math
+
+if __name__ == '__main__':
+    # Ввести список одной строкой.
+    a = tuple(map(int, input().split()))
+    # Если список пуст, завершить программу.
+    if not a:
+        print("Заданный список пуст", file=sys.stderr)
+        exit(1)
+
+    #  2) сумму положительных элементов списка,
+    #       расположенных до максимального элемента.
+    # Вторая часть
+    b = max(a)
+    f = -1
+    e = 0
+    for i in a:
+        f += 1
+        if i == b:
+            e = f
+
+    s = 0
+    for k in range(e):
+        s += a[k]
+    print("Сумма: ", int(s))
+
+    a = tuple(reversed(a))
+    print(a)
